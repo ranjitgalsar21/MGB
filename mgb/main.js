62232af038fed65fc7789c5cd910f849f568d1b2
Atlantiser <atlantiser@Atlantisers-Mac-mini.local>
2026-05-19 17:06:41 +0530

    sticky form code added

--- mgb/main.js
+++ mgb/main.js
@@ -341,6 +341,41 @@ function closeLoader() {
     }
   }, "-=0.2");
 }
+function mobileStickyForm() {
+  if (window.innerWidth > 991) return;
+
+  const toggleBtn = document.querySelector(".mobile-toggle-btn");
+  const form = document.querySelector(".sticky-contact-form");
+  const wrapper = document.querySelector(".sticky_content-wrapper");
+  const buttonText = document.querySelector(".mobile-toggle-btn .sticky_txt");
+  const hideButtons = document.querySelectorAll(".hide-on-form");
+
+  if (!toggleBtn || !form) return;
+
+  // Prevent duplicate listeners
+  if (toggleBtn.dataset.bound === "true") return;
+  toggleBtn.dataset.bound = "true";
+
+  toggleBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    form.classList.toggle("is-open");
+
+    const isOpen = form.classList.contains("is-open");
+
+    wrapper?.classList.toggle("form-open", isOpen);
+
+    hideButtons.forEach((item) => {
+      item.classList.toggle("is-hidden", isOpen);
+    });
+
+    if (buttonText) {
+      buttonText.textContent = isOpen
+        ? "Back"
+        : "Request a call back";
+    }
+  });
+}
 function initStickyStepsBasic() {
   const containers = document.querySelectorAll("[data-sticky-steps-init]");
   if (!containers.length) return;
@@ -482,6 +517,7 @@ document.fonts.ready.then(() => {
     // heroAnimation();
     footerAnimation();
     cardsAnimation();
+    mobileStickyForm();
     
     videoPopup();
     loaderAnimation();
@@ -549,6 +585,7 @@ function updateActiveLink() {
     // updateActiveLink();
     commonFunctions();
     footerAnimation();
+    mobileStickyForm();
     
     sliderReview();
     initStickyStepsBasic();
